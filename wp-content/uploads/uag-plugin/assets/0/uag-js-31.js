98bf4979902ed92e84254523a6bd7b37e7b19ad3
Aleksandar Zivkovic <azivkovi@cisco.com>
2024-06-30 20:19:34 +0200

    Initial commit

---
+++ wp-content/uploads/uag-plugin/assets/0/uag-js-31.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-21fcf598' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-63527ea7' );
+});
+ });
